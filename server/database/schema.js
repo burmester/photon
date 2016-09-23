bd76fd5ec545faca94ee00c14f023c846a499358
Johan Burmester <johan.burmester@unikum.net>
2016-09-23 22:00:39 +0200

    all users, remove pokemon

--- server/database/schema.js
+++ server/database/schema.js
@@ -1,14 +1,8 @@
-import {
-  GraphQLObjectType,
-  GraphQLSchema,
-  GraphQLNonNull,
-  GraphQLList,
-  GraphQLString,
-} from 'graphql';
+import {GraphQLObjectType, GraphQLSchema, GraphQLNonNull, GraphQLList, GraphQLString} from 'graphql';
 
-import { Pokemon } from './Pokemon';
-import { PokemonType, UserType } from './schemaTypes';
-import { db } from './mongoService';
+import {Pokemon} from './Pokemon';
+import {PokemonType, UserType} from './schemaTypes';
+import {db} from './mongoService';
 
 import Q from 'q';
 
@@ -20,6 +14,24 @@ let schema = new GraphQLSchema({
         type: new GraphQLList(PokemonType),
         resolve: () => Pokemon
       },
+      users: {
+        type: new GraphQLList(UserType),
+        resolve: (root) => {
+          return db().then(db => {
+            let deferred = Q.defer();
+            let collection = db.collection('users');
+            collection.find().toArray((err, docs) => {
+              if (err) {
+                deferred.reject(err);
+                return;
+              }
+              db.close();
+              deferred.resolve(docs);
+            })
+            return deferred.promise;
+          })
+        }
+      },
       user: {
         type: UserType,
         args: {
@@ -30,24 +42,24 @@ let schema = new GraphQLSchema({
         },
         resolve: (root, {name}) => {
 
-          return db()
-            .then(db => {
-              let deferred = Q.defer();
+          return db().then(db => {
+            let deferred = Q.defer();
 
-              let collection = db.collection('users');
-              collection.find({ name })
-                .toArray((err, docs) => {
-                  if (err) {
-                    deferred.reject(err);
-                    return;
-                  }
+            let collection = db.collection('users');
+            collection.find({name}).toArray((err, docs) => {
+              if (err) {
+                deferred.reject(err);
+                return;
+              }
 
-                  db.close();
-                  deferred.resolve(docs.length ? docs[0] : null);
-                });
-
-              return deferred.promise;
+              db.close();
+              deferred.resolve(docs.length
+                ? docs[0]
+                : null);
             });
+
+            return deferred.promise;
+          });
 
         }
       }
@@ -73,40 +85,38 @@ let schema = new GraphQLSchema({
             created: new Date().valueOf()
           };
 
-          return db()
-            .then(db => {
-              let deferred = Q.defer();
+          return db().then(db => {
+            let deferred = Q.defer();
 
-              let collection = db.collection('users');
+            let collection = db.collection('users');
 
-              // see if the user already exists
-              collection.find({ name })
-                .toArray((err, docs) => {
-                  if (err) {
-                    db.close();
-                    return deferred.reject(err);
-                  }
+            // see if the user already exists
+            collection.find({name}).toArray((err, docs) => {
+              if (err) {
+                db.close();
+                return deferred.reject(err);
+              }
 
-                  if (docs.length) {
-                    db.close();
-                    return deferred.resolve(docs[0]);
-                  }
+              if (docs.length) {
+                db.close();
+                return deferred.resolve(docs[0]);
+              }
 
-                  // insert the user if they do not exist
-                  collection.insert(toCreate, (err, result) => {
-                    db.close();
+              // insert the user if they do not exist
+              collection.insert(toCreate, (err, result) => {
+                db.close();
 
-                    if (err) {
-                      deferred.reject(err);
-                      return;
-                    }
+                if (err) {
+                  deferred.reject(err);
+                  return;
+                }
 
-                    deferred.resolve(toCreate);
-                  });
-                });
-
-              return deferred.promise;
+                deferred.resolve(toCreate);
+              });
             });
+
+            return deferred.promise;
+          });
 
         }
       },
@@ -123,42 +133,95 @@ let schema = new GraphQLSchema({
           }
         },
         resolve: (obj, {name, pokemon}) => {
-          return db()
-            .then(db => {
-              let deferred = Q.defer();
+          return db().then(db => {
+            let deferred = Q.defer();
 
-              let collection = db.collection('users');
+            let collection = db.collection('users');
 
-              // find the user
-              collection.find({ name })
-                .toArray((err, docs) => {
-                  if (err || !docs.length) {
-                    db.close();
-                    return deferred.reject(err || 'The user was not found');
-                  }
+            // find the user
+            collection.find({name}).toArray((err, docs) => {
+              if (err || !docs.length) {
+                db.close();
+                return deferred.reject(err || 'The user was not found');
+              }
 
-                  let caught = docs[0].caught;
-                  caught.push(pokemon);
+              let caught = docs[0].caught;
+              const p = Pokemon.find((p) => {
+                return p.name === pokemon
+              });
+              if (p)
+                caught.push(p);
 
-                  // update the user with updated caught array
-                  collection.update({ name }, {
-                    $set : { caught }
-                  }, (err, result) => {
-                    if (err) {
-                      db.close();
-                      return deferred.reject(err);
-                    }
+              // update the user with updated caught array
+              collection.update({
+                name
+              }, {
+                $set: {
+                  caught
+                }
+              }, (err, result) => {
+                if (err) {
+                  db.close();
+                  return deferred.reject(err);
+                }
 
-                    deferred.resolve({
-                      name : docs[0].name,
-                      created : docs[0].created,
-                      caught
-                    });
-                  });
-                });
-
-              return deferred.promise;
+                deferred.resolve({name: docs[0].name, created: docs[0].created, caught});
+              });
             });
+
+            return deferred.promise;
+          });
+        }
+      },
+      losePokemon: {
+        type: UserType,
+        args: {
+          name: {
+            description: 'The name of the user',
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          pokemon: {
+            description: 'The name of the Pokemon that got lose',
+            type: new GraphQLNonNull(GraphQLString)
+          }
+        },
+        resolve: (obj, {name, pokemon}) => {
+          return db().then(db => {
+            let deferred = Q.defer();
+
+            let collection = db.collection('users');
+
+            // find the user
+            collection.find({name}).toArray((err, docs) => {
+              if (err || !docs.length) {
+                db.close();
+                return deferred.reject(err || 'The user was not found');
+              }
+
+              let caught = docs[0].caught;
+              caught = caught.filter((p) => {
+                p.name != pokemon
+              });
+
+              // update the user with updated caught array
+              collection.update({
+                name
+              }, {
+                $set: {
+                  caught
+                }
+              }, (err, result) => {
+                if (err) {
+                  db.close();
+                  return deferred.reject(err);
+                }
+
+                deferred.resolve({name: docs[0].name, created: docs[0].created, caught});
+              });
+            });
+
+            return deferred.promise;
+          });
         }
       }
     }
